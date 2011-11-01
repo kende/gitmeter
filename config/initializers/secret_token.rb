e3c4da13193ff3dd3c4b4118edc3bb94a71f8027
kende <kende.j@gmail.com>
2011-10-31 21:51:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitmeter::Application.config.secret_token = 'c43ad96248cd83791c80b9639126c671505b348038671b4b9adaed587942e487b880f1ef42388768c8e2bed5ed99666fd093bfff45b575e985b226b05cdb450a'
